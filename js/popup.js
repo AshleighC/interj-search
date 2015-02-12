0b721dd0d414d403c7232147ac4922f4a98b9ef4
Ashleigh Cushman <ashleigh.cushman@gmail.com>
2015-02-12 04:43:32 -0800

    Fixed minor things.

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,14 @@
 $('#go-button').click(function() {
-  word = $('#word-input').val().toLowerCase();
-  matches = word.match(/\((.)\)/);
+  var word = $('#word-input').val().toLowerCase();
+  var matches = word.match(/\((.)\)/);
 
   if (matches && (word.match(/[\(\)]/g).length == 2)) {
     $('#error').hide();
 
-    message = {
+    var message = {
       'word': word,
       'letter': matches[1]
-    }
+    };
   } else {
     $('#error').show();
   }
